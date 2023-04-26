cdfdd36545f22c5193e2aec4aa132cfefc6cdb85
GulcanAkuklu <gulcanakuklu3@gmail.com>
2023-04-26 21:33:43 +0200

    paralel testing

--- src/test/java/com/eurotech/runners/CukesRunner.java
+++ src/test/java/com/eurotech/runners/CukesRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/Features",
         glue= "com/eurotech/step_definitions",
         dryRun = false,
-       tags = "@smoke"
+       tags = "@rerun"
 
 )
 public class CukesRunner {
